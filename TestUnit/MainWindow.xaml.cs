17ee8b698eebd08998285e34e0b089cca2c4a872
ff_wang <1105936347@qq.com>
2023-04-02 17:57:23 +0800

    :tada:

---
+++ TestUnit/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using WffCalDotNet;
+using WffCalDotNet.Model;
+
+namespace TestUnit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 读取数据
+            double[,] EH = WffCal.LoadMat(Environment.CurrentDirectory + "\\EH_T1T2.txt");
+            double[] tau1 = WffCal.LoadVec(Environment.CurrentDirectory + "\\tau1.txt");
+            double[] tau2 = WffCal.LoadVec(Environment.CurrentDirectory + "\\tau2.txt");
+            int Nw = tau1.Length;
+            int NECH = tau2.Length;
+            int Nt = 64;
+            double Tmin = 0.01;
+            double Tmax = 10000;
+            int invModel = 2;
+            double alpha = 1;
+
+            // 初始化反演参数
+            InvParam param = new InvParam
+            {
+                EH = EH,
+                Tau1 = tau1,
+                Tau2 = tau2,
+                Nw = Nw,
+                NECH = NECH,
+                Nt = Nt,
+                Tmin = Tmin,
+                Tmax = Tmax,
+                InvModel = invModel,
+                Alpha = alpha,
+            };
+
+            // 记录反演时间
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            // 调用接口
+            WffCal.InvT1T2(ref param);
+            watch.Stop();
+            _ = LB_Log.Items.Add(string.Format("反演耗时：{0}ms", watch.ElapsedMilliseconds));
+
+            // 保存反演结果
+            param.T2T1Dist.Save(Environment.CurrentDirectory + "\\T2T1Dist.txt");
+            param.T2Dist.Save(Environment.CurrentDirectory + "\\T2Dist.txt");
+        }
+    }
+}
---
+++ WffCalDotNet/Model/InvParam.cs
@@ -0,0 +1,96 @@
+﻿namespace WffCalDotNet.Model
+{
+    ////
+    /// ----------------------------------------------------------------
+    /// Copyright @BigWang 2023 All rights reserved
+    /// Author      : BigWang
+    /// Created Time: 2023/4/1 21:19:59
+    /// Description :
+    /// ----------------------------------------------------------------
+    /// Version      Modified Time              Modified By     Modified Content
+    /// V1.0.0.0     2023/4/1 21:19:59                     BigWang         首次编写         
+    ///
+    public class InvParam
+    {
+        /// <summary>
+        /// 待反演数据
+        /// 每行为一组数据
+        /// </summary>
+        public double[,] EH { get; set; }
+
+        /// <summary>
+        /// Tw 时间
+        /// </summary>
+        public double[] Tau1 { get; set; }
+
+        /// <summary>
+        /// Tw 个数
+        /// </summary>
+        public int Nw { get; set; }
+
+        /// <summary>
+        /// T2 采集时间
+        /// </summary>
+        public double[] Tau2 { get; set; }
+
+        /// <summary>
+        /// 回波个数
+        /// </summary>
+        public int NECH { get; set; }
+
+        /// <summary>
+        /// 最小弛豫时间
+        /// </summary>
+        public double Tmin { get; set; }
+
+        /// <summary>
+        /// 最大弛豫时间
+        /// </summary>
+        public double Tmax { get; set; }
+
+        /// <summary>
+        /// 正则化因子
+        /// </summary>
+        public double Alpha { get; set; }
+
+        /// <summary>
+        /// 反演点数
+        /// </summary>
+        public int Nt { get; set; }
+
+        /// <summary>
+        /// 反演模型：1表示饱和恢复，2表示反转恢复
+        /// </summary>
+        public int InvModel { get; set; }
+
+        /// <summary>
+        /// 反演结果 T2 -T1 分布
+        /// </summary>
+        public double[,] T2T1Dist { get; set; }
+
+        /// <summary>
+        /// 拟合结果
+        /// </summary>
+        public double[,] EHfit { get; set; }
+
+        /// <summary>
+        /// 弛豫时间
+        /// </summary>
+        public double[] T { get; set; }
+
+        /// <summary>
+        /// T1 分布，累积
+        /// </summary>
+        public double[] T1Dist { get; set; }
+
+        /// <summary>
+        /// T2 分布，累积
+        /// </summary>
+        public double[] T2Dist { get; set; }
+
+        /// <summary>
+        /// 相对误差
+        /// </summary>
+        public double ReError { get; set; }
+    }
+}
